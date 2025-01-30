65db52a3ab6727e5cd4fd905f70aaf561dda413f
Snehal Suman <snehals0203@gmail.com>
2025-01-30 23:42:25 +0530

    Added delete image functionality

--- controllers/image-controller.js
+++ controllers/image-controller.js
@@ -1,5 +1,6 @@
 const Image = require('../models/images.js')
 const uploadToCloudinary = require('../helpers/cloudinaryHelpers.js')
+const cloudinary = require('../config/cloudinary.js')
 
 const uploadImage = async (req, res) => {
 
@@ -40,43 +41,43 @@ const uploadImage = async (req, res) => {
 
 }
 
-const getImage=async(req,res)=>{
+const getImage = async (req, res) => {
 
-    try{
+    try {
 
-        const page=parseInt(req.query.page) || 1
-        const limit=parseInt(req.query.limit) || 5
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 5
 
-        const skip=(page-1)*limit
+        const skip = (page - 1) * limit
 
-        const sortBy=req.query.sortBy || "createdAt"
-        const sortOrder=req.query.sortOrder==='asc'?1:-1
+        const sortBy = req.query.sortBy || "createdAt"
+        const sortOrder = req.query.sortOrder === 'asc' ? 1 : -1
 
-        const totalImages=await Image.countDocuments()
-        const totalPages=Math.ceil(totalImages/limit)
+        const totalImages = await Image.countDocuments()
+        const totalPages = Math.ceil(totalImages / limit)
 
-        const sortObj={}
+        const sortObj = {}
         sortObj[sortBy] = sortOrder
-        
-        const images=await Image.find({}).sort(sortObj).skip(skip).limit(limit)
-        if(!images){
+
+        const images = await Image.find({}).sort(sortObj).skip(skip).limit(limit)
+        if (!images) {
             return res.status(404).json({
-                success:false,
-                message:`No images found`
+                success: false,
+                message: `No images found`
             })
         }
-        
+
         res.status(200).json({
-            success:true,
-            message:`Images found`,
-            currentPage:page,
-            totalImages:totalImages,
-            totalPages:totalPages,
-            data:images
+            success: true,
+            message: `Images found`,
+            currentPage: page,
+            totalImages: totalImages,
+            totalPages: totalPages,
+            data: images
         })
 
 
-    }catch(error){
+    } catch (error) {
 
         console.log(error.message);
         return res.status(500).json({
@@ -88,4 +89,38 @@ const getImage=async(req,res)=>{
 
 }
 
-module.exports = {uploadImage , getImage }
+
+const deleteImage = async (req, res) => {
+
+    try {
+        const imageId = req.params.id
+
+        const getCurrentImageDetails = await Image.findById(imageId)
+        if (!getCurrentImageDetails) {
+            return res.status(404).json({
+                success: false,
+                message: `Image not found`
+            })
+        }
+
+        // delete from cloudinary
+        await cloudinary.uploader.destroy(getCurrentImageDetails.publicId)
+
+        // delete from MongoDB
+        await Image.findByIdAndDelete(imageId)
+
+        res.status(200).json({
+            success: true,
+            message: `Image deleted`
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: `Internal server error`
+        })
+    }
+
+}
+
+module.exports = { uploadImage, getImage, deleteImage }
--- routes/image-route.js
+++ routes/image-route.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const authMiddleware = require('../middleware/auth-middleware.js')
 const adminMiddleware = require('../middleware/admin-middleware.js')
 
-const {uploadImage , getImage }= require('../controllers/image-controller.js')
+const {uploadImage , getImage, deleteImage }= require('../controllers/image-controller.js')
 const uploadMiddleware = require('../middleware/upload-middleware.js')
 
 router.post('/upload', authMiddleware, adminMiddleware, uploadMiddleware.single("image"), uploadImage)
 router.get('/get',authMiddleware,getImage)
+router.delete('/delete/:id',authMiddleware,adminMiddleware,deleteImage)
 
 module.exports = router
